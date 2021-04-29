9395b766d5f66cf562e1a48fcfb4cbc4a2458be3
Samiran Roy <samiranroy0407@gmail.com>
2021-04-29 20:12:17 +0530

    Users craete done

--- controllers/trip_controller.js
+++ controllers/trip_controller.js
@@ -31,3 +31,49 @@ module.exports.destroy = async function(req, res){
         });
     }
 }
+
+module.exports.tripOpen = async function(req, res){
+    try{
+        let trip = await Trip.findById(req.query.id);
+
+        return res.render('trip', {
+            trip: trip
+        });
+    }catch(err){
+        console.log(err);
+        return res.json(500, {
+            message: 'Error in Trip Code'
+        });
+    }
+}
+
+module.exports.createUser = async function(req, res){
+    try{
+        let trip = await Trip.findById(req.body.id);
+        
+        let username = req.body.name;
+
+        for(let user of trip.users)
+        {
+            if(user.name == username)
+            {
+                return res.redirect('back');
+            }
+        }
+
+        let user = {
+            name: username,
+            money: 0
+        }
+
+        trip.users.push(user);
+        trip.save();
+
+        return res.redirect('back');
+    }catch(err){
+        console.log(err);
+        return res.json(500, {
+            message: 'Error in Trip Code'
+        });
+    }
+}
--- routes/trip.js
+++ routes/trip.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const trip_controller = require('../controllers/trip_controller');
 
 router.post('/create', trip_controller.create);
+router.post('/create-user', trip_controller.createUser);
 router.get('/destroy', trip_controller.destroy);
+router.get('/trip-open', trip_controller.tripOpen);
 
 module.exports = router;
