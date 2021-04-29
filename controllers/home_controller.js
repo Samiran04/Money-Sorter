22ce69242bb65a97c7e330f84d522d61f9e1141a
Samiran Roy <samiranroy0407@gmail.com>
2021-04-29 19:52:42 +0530

    trip delete done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,13 @@
+const Trip = require('../models/trip');
+
 module.exports.home = async function(req, res){
     try{
-        return res.render('home');
+
+        let trips = await Trip.find({});
+
+        return res.render('home', {
+            trips: trips
+        });
     }catch(err){
         console.log(err);
         return res.json(500, {
